83c1212798c1a44c66f7e5658883d4d4e0661786
deftangel <deftangel@gmail.com>
2022-07-01 17:55:51 +0100

    Project 1 with challenges

---
+++ project1/project1/ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  project1
+//
+//  Created by Chris Hunter-Brown on 01/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused: Bool
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var grandTotal: Double {
+        let tipPercent = Double(tipPercentage)
+        let orderAmount = Double(checkAmount)
+        
+        let tipValue = orderAmount / 100 * tipPercent
+        let grandTotal = orderAmount + tipValue
+        return grandTotal
+    }
+    
+    let currencyFormatter = FloatingPointFormatStyle<Double>.Currency.currency(code: Locale.current.currencyCode ?? "USD")
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currencyFormatter)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                            ForEach(2 ..< 100) {
+                                Text("\($0) people")
+                            }
+                        }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< 101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section(header:Text("Total with tip")) {
+                    Text("$\(grandTotal, specifier: "%.2f")")
+                        .foregroundColor(self.tipPercentage > 0 ? .blue : .red)
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: currencyFormatter)
+                } header: {
+                    Text("Amount per person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project1/project1/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  project1
+//
+//  Created by Chris Hunter-Brown on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
