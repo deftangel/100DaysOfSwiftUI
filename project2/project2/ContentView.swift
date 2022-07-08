fcd06e08c794864819843fa3f96605be4e7e8c84
deftangel <deftangel@gmail.com>
2022-07-08 17:31:30 +0100

    Project 2 challenges

---
+++ project2/project2/ContentView.swift
@@ -0,0 +1,135 @@
+//
+//  ContentView.swift
+//  project2
+//
+//  Created by Chris Hunter-Brown on 20/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var score = 0
+    @State private var showingGameOver = false
+    @State private var questionNumber = 1
+    
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                
+                    ForEach(0 ..< 3) { number in
+                        Button(action: {
+                            self.flagTapped(number)
+                        }) {
+                            FlagImage(imageName: self.countries[number])
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .fontWeight(.heavy)
+                
+                Spacer()
+            }
+            .padding()
+        }.alert(isPresented: $showingScore, content: {
+            Alert(title: Text(scoreTitle), message: Text(scoreMessage), dismissButton: .default(Text("Continue"), action: {
+                self.checkGameOver()
+            }))
+        })
+        .alert("Game over", isPresented: $showingGameOver) {
+            Button("New game") {
+                self.startNewGame()
+            }
+        } message: {
+            Text("You scored \(score) points")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            score += 1
+            scoreTitle = "Correct"
+            scoreMessage = "Your score is \(score)"
+        } else {
+            score -= 1
+            scoreTitle = "Wrong"
+            scoreMessage = "That's the flag of \(countries[number])"
+        }
+        
+        showingScore = true
+    }
+    
+    func checkGameOver() {
+        questionNumber += 1
+        if questionNumber > 8 {
+            showingGameOver = true
+        } else {
+            askQuestion()
+        }
+    }
+    
+    func startNewGame() {
+        questionNumber = 1
+        score = 0
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+struct FlagImage: View {
+    var imageName: String
+    var body: some View {
+        Image(imageName)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+            .shadow(color: .black, radius: 5)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project2/project2/project2App.swift
@@ -0,0 +1,17 @@
+//
+//  project2App.swift
+//  project2
+//
+//  Created by Chris Hunter-Brown on 20/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
