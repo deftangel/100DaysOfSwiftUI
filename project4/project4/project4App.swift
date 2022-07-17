dd0e59afd9aa1bdf77b71b2fb264ecb9b4bba093
deftangel <deftangel@gmail.com>
2022-07-17 23:40:45 +0100

    Project 4

---
+++ project4/project4/project4App.swift
@@ -0,0 +1,17 @@
+//
+//  project4App.swift
+//  project4
+//
+//  Created by Chris Hunter-Brown on 09/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
