df9cda656a72dbdb77787b46f5216e06710d6908
deftangel <deftangel@gmail.com>
2023-04-19 23:19:09 +0100

    Project 5

---
+++ project5/project5/project5App.swift
@@ -0,0 +1,17 @@
+//
+//  project5App.swift
+//  project5
+//
+//  Created by Chris Hunter-Brown on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct project5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
