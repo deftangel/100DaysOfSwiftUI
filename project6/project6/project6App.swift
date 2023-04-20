964543fa6e3ada69581d823fb8df558c42b7f9ba
deftangel <deftangel@gmail.com>
2023-04-20 22:08:57 +0100

    Project 6

---
+++ project6/project6/project6App.swift
@@ -0,0 +1,17 @@
+//
+//  project6App.swift
+//  project6
+//
+//  Created by Chris Hunter-Brown on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct project6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
