dbdd932624b6e71868f6f9e53c864e176d7cb20e
deftangel <deftangel@gmail.com>
2023-04-25 16:15:39 +0100

    Project 7

---
+++ project7/project7/project7App.swift
@@ -0,0 +1,17 @@
+//
+//  project7App.swift
+//  project7
+//
+//  Created by Chris Hunter-Brown on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct project7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
