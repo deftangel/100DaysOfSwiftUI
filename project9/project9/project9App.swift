b682ad2039fb6f474d1f3e986d882472767f73df
deftangel <deftangel@gmail.com>
2024-01-16 11:04:37 +0000

    Project 9

---
+++ project9/project9/project9App.swift
@@ -0,0 +1,17 @@
+//
+//  project9App.swift
+//  project9
+//
+//  Created by Chris Hunter-Brown on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct project9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
