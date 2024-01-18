f9835f5aa184d8a52a11f31a1cfc13f58a5df54c
deftangel <deftangel@gmail.com>
2024-01-18 11:58:25 +0000

    Update project8 for iOS 17

--- project8/Moonshot/AstronautView.swift
+++ project8/Moonshot/AstronautView.swift
@@ -28,11 +28,9 @@ struct AstronautView: View {
     }
 }
 
-struct AstronautView_Previews: PreviewProvider {
-    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
 
-    static var previews: some View {
-        AstronautView(astronaut: astronauts["aldrin"]!)
-            .preferredColorScheme(.dark)
-    }
+    return AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
 }
--- project8/Moonshot/MoonshotApp.swift
+++ project8/Moonshot/MoonshotApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct project8App: App {
+struct MoonshotApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
